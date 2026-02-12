aa96de12f903555bda6b42ec29260b11366d78ab
MeetGJethva <23ceubd004@ddu.ac.in>
2026-02-12 10:12:21 +0530

    changed to groq

--- src/agentic/llms/primary.py
+++ src/agentic/llms/primary.py
@@ -1,14 +1,14 @@
 import os
-from RAW.llms import GeminiLLM
+from RAW.llms import GroqLLM
 from src.utils import logger
 
-def get_primary_llm() -> GeminiLLM:
+def get_primary_llm() -> GroqLLM:
     """
     Returns the primary LLM instance (Groq).
     """
-    api_key = os.environ.get("GEMINI_API_KEY")
+    api_key = os.environ.get("GROQ_API_KEY")
     if not api_key:
-        logger.error("GEMINI_API_KEY not found in environment.")
+        logger.error("GROQ_API_KEY not found in environment.")
     
-    model_name = os.environ.get("GEMINI_MODEL", "gemini-2.5-flash")
-    return GeminiLLM(api_key=api_key, logger=logger, model=model_name)
+    model_name = os.environ.get("GROQ_MODEL", "openai/gpt-oss-120b")
+    return GroqLLM(api_key=api_key, logger=logger, model=model_name)
